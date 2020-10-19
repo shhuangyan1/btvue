aea3ed4ecc3a6d39dfbf875dbd8c6b7f195760ce
shhuangyan1 <shhuangyan1@163.com>
2020-10-19 11:33:29 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+        <p>For more information visit website</p>
+        <b-button variant="primary" href="#">More Info</b-button>
+      </b-jumbotron>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
